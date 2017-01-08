2ae3cd0969dce3fdf3f8c9d0ce406c6c8756103f
Reuben Ajayi <reubenajayilsus@gmail.com>
2017-01-07 19:44:29 -0600

    Update Users database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:CompanyName, :CompanyEmail, :Stock)
+      params.require(:user).permit(:User, :Password, :CompanyName, :CompanyEmail, :Stock)
     end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :CompanyName, :CompanyEmail, :Stock, :created_at, :updated_at
+json.extract! user, :id, :User, :Password, :CompanyName, :CompanyEmail, :Stock, :created_at, :updated_at
 json.url user_url(user, format: :json)
