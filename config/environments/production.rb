fd243a24da2f59446d702b2493cb3c2f030a5911
Reuben Ajayi <reubenajayilsus@gmail.com>
2017-01-23 20:59:40 -0600

    Remove Google api fonts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,13 +21,14 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_assets = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
