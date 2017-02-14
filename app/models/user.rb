8224d2fad8dc42abbbae39e18ba1ac4b15cdb717
kenny nguyen <kenny@gmail.com>
2017-02-13 21:25:06 -0600

    Some small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
 	validates :first_name, length: { maximum: 50 }, presence: true
 	validates :last_name, length: { maximum: 50 }, presence: true
 	validates :email, length: { maximum: 50 }, presence: true
-	validates :password, length: { minimum: 8 }, presence: true
+	validates :password, length: { maximum: 16 }, presence: true
+	has_secure_password
 end
